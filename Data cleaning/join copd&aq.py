3e32d1740c8701c50e8756b38c638bdc5aa29515
Haru-Tachibana <paloma030415@gmail.com>
2024-10-10 15:20:45 +0100

    new py file to merge aq data and copd data

---
+++ Data cleaning/join copd&aq.py
@@ -0,0 +1,9 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+copd = pd.read_csv('/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/copd_joint.csv')
+aq18 = pd.read_csv('/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/aq18.csv')
