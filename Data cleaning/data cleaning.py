ae8d24c02c5b119a006c0b44419e6768ebf810a0
Haru-Tachibana <paloma030415@gmail.com>
2024-10-10 13:39:59 +0100

    update

--- Data cleaning/data cleaning.py
+++ Data cleaning/data cleaning.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 
 copd_age = pd.read_csv("/Users/yangyangxiayule/Documents/GitHub/COPD-Project/Datasets/COPD data(csv)/AGE-ADJUSTED_PREVALENCE_COPD.csv")
 copd_crude = pd.read_csv("/Users/yangyangxiayule/Documents/GitHub/COPD-Project/Datasets/COPD data(csv)/CRUDE_PREVALENCE_COPD.csv")
@@ -23,5 +24,28 @@ smoke_crude = pd.read_csv("/Users/yangyangxiayule/Documents/GitHub/COPD-Project/
 no_phyact_1821_age = pd.read_csv("/Users/yangyangxiayule/Documents/GitHub/COPD-Project/Datasets/lifestyle risk factors/NO_PHYSICAL_ACTIVITY_AGE-ADJUSTED.csv")
 no_phyact_1821_crude = pd.read_csv("/Users/yangyangxiayule/Documents/GitHub/COPD-Project/Datasets/lifestyle risk factors/NO_PHYSICAL_ACTIVITYAGE_Crude.csv")
 
-copd_joint = pd.merge(copd_age, copd_crude, how='inner', on=['County', 'Year'])
-copd_joint.to_csv("/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/copd_joint.csv", index=False)
+
+years_copd = copd_age['Year'].unique()
+print(years_copd)
+copd_age.rename(columns={
+    'Value': 'copd%_age',
+    'Confidence Interval High': 'CI_high_copd_age',
+    'Confidence Interval Low': 'CI_low_copd_age',
+}, inplace=True)
+copd_age.drop(columns=['Data Comment', '95% Confidence Interval'], inplace=True)
+copd_age.dropna(axis=1, inplace=True)
+copd_age.dropna(axis=0, inplace=True)
+
+copd_crude.rename(columns={
+    'Value': 'copd%_crude',
+    'Confidence Interval High': 'CI_high_copd_crude',
+    'Confidence Interval Low': 'CI_low_copd_crude',
+}, inplace=True)
+copd_crude.drop(columns=['StateFIPS', 'State', 'Data Comment', '95% Confidence Interval'], inplace=True)
+copd_crude.dropna(axis=1, inplace=True)
+copd_crude.dropna(axis=0, inplace=True)
+
+print(copd_age.columns.to_list())
+print(copd_crude.columns.to_list())
+#copd_joint = pd.merge(copd_age, copd_crude, how='inner', on=['County', 'Year'])
+#copd_joint.to_csv("/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/copd_joint.csv", index=False)
