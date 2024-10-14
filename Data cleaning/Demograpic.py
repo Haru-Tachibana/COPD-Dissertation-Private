50664075e32bdbd414bc0315aaf4e056d8793bb7
Haru-Tachibana <paloma030415@gmail.com>
2024-10-14 17:16:05 +0100

    joint SD copd + SD income
    2021 median income data missing

--- Data cleaning/Demograpic.py
+++ Data cleaning/Demograpic.py
@@ -37,3 +37,16 @@ print(income_ind.head())
 
 SD_income.to_csv('/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/SD_income.csv', index=False)
 income_ind.to_csv('/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/income_ind.csv', index=False)
+
+copd = pd.read_csv('/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/copd_joint.csv')
+copd_income = pd.merge(copd, income_ind, how='left', on=['State_County_Year'])
+
+copd_income.rename(columns={'Value': 'Median_Income'}, inplace=True)
+
+print(copd_income.head())
+print(copd_income.columns.to_list())
+
+copd_income_SD = copd_income[copd_income["StateFIPS"] == 46]
+
+#2021 income data missing
+copd_income_SD.to_csv('/Users/yangyangxiayule/Documents/GitHub/COPD-Project/New CSV/copd_income_SD.csv', index=False)
